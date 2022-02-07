490c7e623f7b2c9187b4ffe7499b8207057290aa
天山来客 <hengwujun128@gmail.com>
2022-02-07 15:25:09 +0800

    feat:  change pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   apps : [{
+    name: "myAPI",
     script: 'app/index.js',
-    watch: './app'
+    watch: 'true', // Restart on file change
+    watch_delay: 1000,
+    // Specify which folder to ignore 
+    ignore_watch : ["node_modules"],
+    instances: 2,
+    exec_mode: "cluster",
+    increment_var : 'PORT', // PM2 will see that i want to increment the PORT variable for each instance;The first instance will have process.env.PORT = 3000 and the second process.env.PORT = 3001
+    // default environment
+    env: {
+      "PORT": 3000,
+      "NODE_ENV": "development"
+    },
+    // to use env_production by using pm2 start ecosystem.config.js --env production.
+    env_production: {
+      "PORT": 4000,
+      "NODE_ENV": "production",
+    }
   }],
 
   deploy : {
