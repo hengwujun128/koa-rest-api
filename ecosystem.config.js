67a996e2d42263cd3ae635c2a9d00fe799d98785
Martin <hengwujun128@gmail.com>
2023-06-29 15:48:13 +0800

    feat: add private key path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -31,7 +31,7 @@ module.exports = {
   deploy: {
     development: {},
     production: {
-      key: '/Users/martin/.ssh/id_ed25519.pub', // path to the public key to authenticate
+      key: '/Users/martin/.ssh/TecentCloud.pem', // path to the public key to authenticate
       user: 'ubuntu',
       host: ['119.29.68.169'], // 可以发布到多台机器
       ref: 'origin/master',
